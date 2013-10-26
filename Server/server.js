1c50c3a7ee528ba3383b37b2c3e95dec2b76ff79
Michael Lyons <michael.j.lyons22@gmail.com>
2013-10-26 18:33:44 -0400

    added node packages

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
-var express = require("express");
+var express = require("express"),
+    io = require("socket.io").listen(3001);
 
 app = module.exports = express();
 
@@ -6,3 +7,13 @@ app.get("/", function(req,res) {
     res.writeHead(200, "text/plain");
     res.end("okay");
 });
+
+// socket.io code
+
+var clients = [];
+
+io.sockets.on("connect", function(socket) {
+    // new client trying to connect
+    clients.push(socket);
+})
+
